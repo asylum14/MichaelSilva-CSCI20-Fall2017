ed3738f10a291db384835eab5e5ce1cb77803deb
Michael Silva <msilva015@student.butte.edu>
2017-08-28 17:15:16 +0000

    Lab1.2 Commit

---
+++ lab12/lab12.cpp
@@ -0,0 +1,23 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<" <>=======()"<<endl;
+    cout<<"(/\\___   /|\\\\          ()==========<>"<<endl;
+    cout<<"      \\_/ | \\\\        //|\\   ______/ \\)"<<endl;
+    cout<<"        \\_|  \\\\      // | \\_/"<<endl;
+    cout<<"         \\|\\/|\\_   //   /\\/"<<endl;
+    cout<<"          (oo)\\  \\_//  /"<<endl;
+    cout<<"         //_/\\_\\/  /  |"<<endl;
+    cout<<"        @@/  |=\\   \\  |"<<endl;
+    cout<<"              \\_=\\_ \\ |"<<endl;
+    cout<<"                \\==\\ \\|\\_"<<endl;
+    cout<<"             __(\\===\\(  )\\"<<endl;
+    cout<<"            (((~) __(_/   |"<<endl;
+    cout<<"                 (((~)\\  /"<<endl;
+    cout<<"                 _____/ /"<<endl;
+    cout<<"                 '------'"<<endl;
+    
+}
