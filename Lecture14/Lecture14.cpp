a503fe597df73900a2f905c8f321fce0035ff12c
Michael Silva <msilva015@student.butte.edu>
2017-08-30 20:40:52 +0000

    Lecture1.4 Submission

--- Lecture14/Lecture14.cpp
+++ Lecture14/Lecture14.cpp
@@ -1,43 +1,19 @@
-/* Created By: April Browne
- * Created On: 7/3/2017
- */
-
-#include <iostream>
-using namespace std;
-
-int main()
-{
-    int one = 1;
-    int two = 2;
-    int three = 3;
-    int answer = 0;
-    
-    answer = one+one;
-    cout<<answer<<endl;
-    
-    answer = three - one;
-    cout<<answer<<endl;
-    
-    answer = two*three;
-    cout<<answer<<endl;
-    
-    answer = three/two;
-    cout<<answer<<endl;
-}
-
-//What are the answers to each equation?
-/*
-equation 1 = 1+1
-equation 2 = 3-1
-equation 3 = 2*3
-equation 4 = 3/2
-*/
-
-
-//Comment this code with the algorithm.
-/*
-1.Get variables
-2. add/subtract/multiply/ divide accordingly to the equation
-3. print answer
-4. Repeat till done.
-*/
+//Created By: April Browne
+//Created On: 8/22/2016
+ 
+ #include <iostream>
+ using namespace std;
+ 
+ int main()
+ {
+     int studentID = 0;
+     cout<<"Please enter a student ID number: ";
+     cin >> studentID;
+ 
+     cout<<"Hello World! "<<studentID<<endl;
+ }
+ 
+//Identify with a //* 2-3 items that seem important
+//Identify with a //? 2-3 items that seem incorrect, unclear or where you have questions
+//Identify the different parts of the program.
+//Create comments that show the algorithm of the program.
