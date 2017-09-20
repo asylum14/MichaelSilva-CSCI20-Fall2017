883eb721b118656e884355cfa68650813194c0bb
Michael Silva <msilva015@student.butte.edu>
2017-09-20 19:23:14 +0000

    Submissions section2

--- Section2/lab21/lab21.cpp
+++ Section2/lab21/lab21.cpp
@@ -15,13 +15,14 @@ int main(){
     double tempeture=0;
     double old_wind_chill=0;
     double new_wind_chill=0;
-    double velocity_power_constant=pow(velocity,0.16);//I made this variable to simplify the code a bit so there isn't a buch of functions in one equation.
+    double velocity_power_constant=0;
+    //I made this variable to simplify the code a bit so there isn't a buch of functions in one equation.
     double difference=0;
      cout<<"What is the tempeture?"<<endl;
     cin>>tempeture;
     cout<<"How fast is the wind?"<<endl;
     cin>>velocity;
-   
+    velocity_power_constant=pow(velocity,0.16);
     
     old_wind_chill=0.081*(3.71*sqrt(velocity)+(5.81-0.25*velocity))*(tempeture-91.4)+91.4;
     new_wind_chill=35.74+0.6215*tempeture-35.75*velocity_power_constant+0.4275*tempeture*velocity_power_constant;
@@ -29,5 +30,8 @@ int main(){
     
     cout<<"Wind Speed       "<<"Tempeture                "<<"Old Wind Chill Formula       "<<"New Wind Chill Formula"<<"       difference"<<endl;
     cout<<velocity<<"                "<<tempeture<<"                       "<<old_wind_chill<<"                       "<<new_wind_chill<<"                       "<<difference<<endl;
-   
+   cout<<35.74+0.6215*tempeture-35.75<<endl;
+   cout<<velocity_power_constant+0.4275<<endl;
+    cout<<tempeture*velocity_power_constant<<endl;
+    
 }
