322638d835303b5294740de71ab30e500879ca99
Michael Silva <msilva015@student.butte.edu>
2017-11-29 19:44:18 +0000

    Lab52 Submission

---
+++ Section5/lab52/class.h
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+class MyClass {
+ public:
+  /*
+   * Constructor
+   * Initialize our member variable num_ to 0
+   */
+  MyClass();
+  /*
+   * Output our member variable using cout
+   */
+  void Output();
+  void SetNumber(int num);
+  int GetNumber();
+ private:
+  int num_;
+};
